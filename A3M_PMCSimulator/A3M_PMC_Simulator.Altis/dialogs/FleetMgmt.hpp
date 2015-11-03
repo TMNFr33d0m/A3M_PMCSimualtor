04997f710e94b5749bbfcd8f057f510ccd65db92
MikeSwuft <mihail.kazaryan@yandex.ru>
2015-11-03 14:27:26 +0300

    Update FleetMgmt.hpp

--- A3M_PMCSimulator/A3M_PMC_Simulator.Altis/dialogs/FleetMgmt.hpp
+++ A3M_PMCSimulator/A3M_PMC_Simulator.Altis/dialogs/FleetMgmt.hpp
@@ -55,7 +55,7 @@ class A3M_StoreFrame: RscFrame
 	Moving = 1; 
 
 	idc = 1800;
-	text = "Internet Voyager V 1.69.420"; //--- ToDo: Localize;
+	text = "Яндекс Браузер V 1.69.420"; //--- ToDo: Localize;
 	x = 0.101173 * safezoneW + safezoneX;
 	y = 0.0819149 * safezoneH + safezoneY;
 	w = 0.7794 * safezoneW;
@@ -67,17 +67,17 @@ class A3M_Exitbutton: RscButton
 	action = "closeDialog 0;"
 
 	idc = 1600;
-	text = "Exit"; //--- ToDo: Localize;
+	text = "Выход"; //--- ToDo: Localize;
 	x = 0.831245 * safezoneW + safezoneX;
 	y = 0.86309 * safezoneH + safezoneY;
 	w = 0.0401102 * safezoneW;
 	h = 0.0330092 * safezoneH;
-	tooltip = "Exit the Store"; //--- ToDo: Localize;
+	tooltip = "Выйти из магазина"; //--- ToDo: Localize;
 };
 class A3M_BalanceTitle: RscText
 {
 	idc = 1000;
-	text = "Operation Budget Balance:"; //--- ToDo: Localize;
+	text = "Бюджет операции:"; //--- ToDo: Localize;
 	x = 0.603156 * safezoneW + safezoneX;
 	y = 0.0973005 * safezoneH + safezoneY;
 	w = 0.131787 * safezoneW;
@@ -95,7 +95,7 @@ class A3M_BudgetBalance: RscStructuredText
 class A3M_TotalTitle: RscText
 {
 	idc = 1001;
-	text = "Total Operation Cost:"; //--- ToDo: Localize;
+	text = "Итоговая цена операции:"; //--- ToDo: Localize;
 	x = 0.622641 * safezoneW + safezoneX;
 	y = 0.147913 * safezoneH + safezoneY;
 	w = 0.108871 * safezoneW;
@@ -113,7 +113,7 @@ class A3M_TotalBalance: RscStructuredText
 class A3M_RankDisplay: RscText
 {
 	idc = 1002;
-	text = "Your Rank Info:"; //--- ToDo: Localize;
+	text = "Информация о ранге:"; //--- ToDo: Localize;
 	x = 0.649003 * safezoneW + safezoneX;
 	y = 0.191924 * safezoneH + safezoneY;
 	w = 0.080218 * safezoneW;
@@ -133,7 +133,7 @@ class A3M_SelectWheeled: RscButton
 	action = "[] call DoAddWheel"; 
 
 	idc = 1604;
-	text = "Auto Dealership"; //--- ToDo: Localize;
+	text = "Доставка автомобилей"; //--- ToDo: Localize;
 	x = 0.188804 * safezoneW + safezoneX;
 	y = 0.422984 * safezoneH + safezoneY;
 	w = 0.0898764 * safezoneW;
@@ -145,7 +145,7 @@ class A3M_SelectArmor: RscButton
 	action = "[] call DoAddArmored";
 
 	idc = 1605;
-	text = "Armor Depot"; //--- ToDo: Localize;
+	text = "Дэпо обслуживания бронетехникиt"; //--- ToDo: Localize;
 	x = 0.107916 * safezoneW + safezoneX;
 	y = 0.422984 * safezoneH + safezoneY;
 	w = 0.0730246 * safezoneW;
@@ -157,19 +157,19 @@ class A3M_RotaryVeh: RscButton
 	action = "[] call DoAddRotary";
 
 	idc = 1606;
-	text = "Helicopters"; //--- ToDo: Localize;
+	text = "Вертолеты"; //--- ToDo: Localize;
 	x = 0.356199 * safezoneW + safezoneX;
 	y = 0.422984 * safezoneH + safezoneY;
 	w = 0.0674073 * safezoneW;
 	h = 0.0330067 * safezoneH;
-	tooltip = "Browse our Rotary Aircraft Fleet"; //--- ToDo: Localize;
+	tooltip = "Просмотрите вертолетный флот"; //--- ToDo: Localize;
 };
 class A3M_SelectPlanes: RscButton
 {
 	action = "[] call DoAddFixedWing";
 
 	idc = 1607;
-	text = "Aircraft"; //--- ToDo: Localize;
+	text = "Самолеты"; //--- ToDo: Localize;
 	x = 0.286543 * safezoneW + safezoneX;
 	y = 0.422984 * safezoneH + safezoneY;
 	w = 0.0617901 * safezoneW;
@@ -192,17 +192,17 @@ class A3M_VehMod: RscCombo
 	onLBSelChanged = "_this spawn A3M_fnc_handleModClick";
 	
 	idc = 2175;
-	text = "Supported Vehicle Mods"; //--- ToDo: Localize;
+	text = "Поддерживаемые моды на технику"; //--- ToDo: Localize;
 	x = 0.658396 * safezoneW + safezoneX;
 	y = 0.315162 * safezoneH + safezoneY;
 	w = 0.206312 * safezoneW;
 	h = 0.0330081 * safezoneH;
-	tooltip = "Select a supported modification, then click a category"; //--- ToDo: Localize;
+	tooltip = "Выберите поддерживаемую модификацию, затем кликните на категорию"; //--- ToDo: Localize;
 };
 class A3M_ModTxt: RscText
 {
 	idc = 1003;
-	text = "Supported Modifications: "; //--- ToDo: Localize;
+	text = "Поддержеваемые модификации: "; //--- ToDo: Localize;
 	x = 0.655024 * safezoneW + safezoneX;
 	y = 0.279955 * safezoneH + safezoneY;
 	w = 0.149003 * safezoneW;
@@ -221,48 +221,48 @@ class A3M_Btn_Prch: RscButton
 {
 	action="[] call A3M_Fnc_VBuyButton";
 	idc = 1601;
-	text = "Purchase"; //--- ToDo: Localize;
+	text = "Заказать"; //--- ToDo: Localize;
 	x = 0.797716 * safezoneW + safezoneX;
 	y = 0.422984 * safezoneH + safezoneY;
 	w = 0.0674073 * safezoneW;
 	h = 0.0330067 * safezoneH;
-	tooltip = "Purchase selected asset(s) "; //--- ToDo: Localize;
+	tooltip = "Заказать выбранное "; //--- ToDo: Localize;
 };
 class A3M_Btn_Robotics: RscButton
 {
 	action = "[] call DoAddRobotics";
 
 	idc = 1602;
-	text = "Robotics"; //--- ToDo: Localize;
+	text = "Робототехника и БПЛА"; //--- ToDo: Localize;
 	x = 0.430347 * safezoneW + safezoneX;
 	y = 0.422984 * safezoneH + safezoneY;
 	w = 0.0674073 * safezoneW;
 	h = 0.0330067 * safezoneH;
-	tooltip = "Autonomous Equipment"; //--- ToDo: Localize;
+	tooltip = "Автономное оборудование"; //--- ToDo: Localize;
 };
 class A3M_Btn_Sprt: RscButton
 {
 	action = "[] call DoAddSupport";
 
 	idc = 1603;
-	text = "Support"; //--- ToDo: Localize;
+	text = "Поддержка"; //--- ToDo: Localize;
 	x = 0.503369 * safezoneW + safezoneX;
 	y = 0.422984 * safezoneH + safezoneY;
 	w = 0.0617901 * safezoneW;
 	h = 0.0330067 * safezoneH;
-	tooltip = "Fleet and Personnel Support "; //--- ToDo: Localize;
+	tooltip = "Флот и персональная поддержка "; //--- ToDo: Localize;
 };
 class A3M_Btn_Upgrades: RscButton
 {
 	action = "[] call DoAddUpgrades";
 
 	idc = 1608;
-	text = "Upgrades"; //--- ToDo: Localize;
+	text = "Улучшения"; //--- ToDo: Localize;
 	x = 0.570776 * safezoneW + safezoneX;
 	y = 0.422984 * safezoneH + safezoneY;
 	w = 0.0674073 * safezoneW;
 	h = 0.0330067 * safezoneH;
-	tooltip = "Base Upgrades"; //--- ToDo: Localize;
+	tooltip = "Улучшения базы"; //--- ToDo: Localize;
 };
 ////////////////////////////////////////////////////////
 // GUI EDITOR OUTPUT END
